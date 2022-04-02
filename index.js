58d1917a252ff642396c208a1510006f00984879
bcaudillo <btcaudillo@gmail.com>
2022-04-02 14:41:56 -0600

    commit tree 3658b416a73ff7686e69a4da91c401b0eb494dbb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+/*
+function isWin(record){
+    return record.result ==="W"
+}
+*/
+
+
+
+
+
+function superbowlWin(records){
+    let x = records.find((record)=>record.result==="W");
+    return x===undefined ? x : x.year
+
+
+
+
+
+   /* if(x === undefined){
+        return undefined        
+    }
+    else{
+        return x.year
+    }
+
+  */  
+}
+
+
